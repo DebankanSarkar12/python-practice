bd3ccf4885254f69e3cb89a653b21957ccd8cab0
DebankanSarkar12 <debankansarkar.kol@gmail.com>
2021-07-22 21:44:37 +0530

    Introduction to printing functions and basic operation

--- hello.py
+++ hello.py
@@ -1,2 +1,35 @@
 # Running our first python prog 
 print("Hello World ")
+
+# Printing our basic calculation
+
+print(2 + 4)    # add 
+print(12 - 5)   # subs
+print(6 * 4)    # multi 
+print(4 / 2)    # div
+print(5 // 3)   # div with no decimal places 
+print(6 % 2)    # modulo operator in python 
+
+#Printing strings in python
+ 
+print("hello this is me krov ")  #hello this is me krov
+print('enclosing the string in single quote')   #enclosing the string in single quote
+print("enclosing a ' single ' quote in a double quoted string  ")   #enclosing a ' single ' quote in a double quoted string 
+print('enclosing a "double " quote string in a single quote string ')  #enclosing a "double " quote string in a single quote string
+print("hello" + "quote")  # string concatination in python 
+
+#Declaring a variable and passing it through the string 
+name = "krov"
+print( "Good morning " + " " + name)   
+
+# taking input from user and passing it through the print statement 
+human = input("Enter your name ")
+print("nice to meet u "+ human)
+#  expected output :Enter your name debankan
+#                  nice to meet u debankan
+
+
+
+
+
+
