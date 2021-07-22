2275b9e08f2be76325183827e72aec0edd46cfc6
DebankanSarkar12 <debankansarkar.kol@gmail.com>
2021-07-22 22:48:48 +0530

    Escape Character in Python

---
+++ escapechar.py
@@ -0,0 +1,19 @@
+
+# \\	        Prints Backslash	
+# \`	        Prints single-quote
+# \"	        Prints double quote	
+# \a	        ASCII bell makes ringing the bell alert sounds ( eg. xterm )	
+# \b	        ASCII backspace ( BS ) removes previous character	
+# \f	        ASCII formfeed ( FF )	
+# \n	        ASCII linefeed ( LF )	
+#  \N{name}	    Prints a character from the Unicode database	
+#  \r	        ASCII carriage return (CR). Moves all characters after ( CR ) the the beginning of the line while overriding same number of characters moved.
+#  \t	        ASCII horizontal tab (TAB). Prints TAB	
+#  \t	        ASCII vertical tab (VT).	
+# \uxxxx	    Prints 16-bit hex value Unicode character	
+# \Uxxxxxxxx	Prints 32-bit hex value Unicode character	
+# \ooo	        Prints character based on its octal value	
+# \xhh      	Prints character based on its hex value	
+
+
+
