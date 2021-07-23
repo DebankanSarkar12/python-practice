0a7dcb8e888dccea4f54cee23e3571d6593eab67
DebankanSarkar12 <debankansarkar.kol@gmail.com>
2021-07-23 19:38:39 +0530

    Datatypes and Examples of numeric datatypes in py

---
+++ datatype.py
@@ -0,0 +1,38 @@
+# Python has several built in data types , that can be classified as 
+#numeric 
+#iterator 
+#sequence(which are also known as iterators )
+#mapping
+#file 
+#class
+#exception
+
+#----------------------------------------------------------------------------------
+ # python3 has three numeric data types :
+# Integers – This value is represented by int class. It contains positive or negative whole numbers (without fraction or decimal). In Python there is no limit to how long an integer value can be.
+# Float – This value is represented by float class. It is a real number with floating point representation. It is specified by a decimal point. Optionally, the character e or E followed by a positive or negative integer may be appended to specify scientific notation. 
+ 
+# Complex Numbers – Complex number is represented by complex class. It is specified as (real part) + (imaginary part)j. For example – 2+3j 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ # int are just whole numbers- numbers that have no fractional part :float is another name for real numbers - numvers that are having a fractional part after the decimal point .
+ # integers are faster than floating point numbers.
+ # floating numbers ------ the maximum float value on a 64 bit computer is 1.7976931348......which means move the decimal point 308 places right.
+ # ------------------------the manimum float value on a 64 bit computer is 307 zeros before the decimal point .
+ # Python float have 52 digits of precision acqurate for most purpose 
+ #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ # Python program to
+
+# demonstrate numeric value
+ 
+a = 5
+print("Type of a: ", type(a))
+ 
+b = 5.0
+print("\nType of b: ", type(b))
+ 
+c = 2 + 4j
+print("\nType of c: ", type(c))
+
+# other datatypes will be seen  in the later modules 
