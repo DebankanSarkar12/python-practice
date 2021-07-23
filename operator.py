23a8f116a357d59a2601b3bb06f1912d352529c8
DebankanSarkar12 <debankansarkar.kol@gmail.com>
2021-07-23 20:16:35 +0530

    operators in py

---
+++ operator.py
@@ -0,0 +1,26 @@
+# This tutorial is about the operators in python 
+a=62
+b=15 
+print(a + b)    # 77 add 
+print(a - b)    # 47 subs
+print(a * b)    # 930 multi 
+print(a / b)    # 4.133333333333334 div
+print(a // b)   # 4 rounded up towards minus infinity 
+print(a % b)    # 2 modulo operator in python ( remainder after integer division )
+print("\n")
+#-----------------------------------------------------------------------------------------------------------
+ #lets seee an example 
+for i in range(1,12//3):         # it is equal to for i in range(1,4).....the value of i will go from 1 to 3 
+    print(i)
+
+#the above prog is similar to 
+print("\n")
+i = 1 
+print(i)
+i = 2 
+print(i)
+i = 3 
+print(i)
+
+# but looping can make a code efficient so thats where loop comes in picture 
+
